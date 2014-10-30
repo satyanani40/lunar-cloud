80224751ee5b204c9127c622054c93eb942e594d
Ratanraj Ravuri <ratanraj.r@gmail.com>
2014-10-30 18:27:39 +0530

    notebooks and generator

---
+++ framework/generator/generator.py
@@ -0,0 +1,26 @@
+import urllib2
+import json
+import time
+import random
+
+randomuser = urllib2.urlopen('http://api.randomuser.me/?results=100')
+results = json.loads(randomuser.read())
+users=results['results']
+
+uids=[i['seed'] for i in users]
+
+fp=open('interests.txt','r')
+interests = []
+for i in fp:
+    interests.append(i.strip())
+
+
+for user in users:
+    friends=set(random.sample(uids,int(random.random()*10)))
+    friends-set([user['seed']])
+
+    user['friends']=list(friends)
+    user['interests']=random.sample(interests,int(random.random()*20))
+
+print json.dumps(users)
+
